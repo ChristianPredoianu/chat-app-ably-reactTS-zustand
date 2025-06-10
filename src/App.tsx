10f13f3cd56ce17c9403d2e1c06b132154d93131
Christian Predoianu <christianpredoianu@yahoo.com>
2025-06-10 12:24:31 +0200

    work

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,29 @@ import { useState } from 'react';
 import ConnectionStatus from './components/ConnectionStatus';
 import RoomStatus from './components/RoomStatus';
 import ChatBox from './components/ChatBox';
+import UsersList from './components/users/UsersList';
 import { ChatRoomProvider } from '@ably/chat/react';
 
-const currentUserId = 'user1';
+const currentUserId = 'my-first-client';
 
-const users = ['user1', 'user2', 'user3'];
+const users = ['my-first-client', 'user2', 'user3'];
 
 export default function App() {
   const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
-  const roomName = selectedUserId && [currentUserId, selectedUserId].sort().join('_');
+
+  const roomName = selectedUserId ? [currentUserId, selectedUserId].sort().join('_') : '';
 
   return (
     <div className='flex w-[900px] h-full mx-auto font-sans border border-blue-500 rounded-lg overflow-hidden'>
-      {/* Users list */}
       <div className='w-1/4 border-r border-blue-500 p-4'>
         <h2 className='text-xl font-bold mb-4'>Users</h2>
-        {users
-          .filter((user) => user !== currentUserId)
-          .map((user) => (
-            <div
-              key={user}
-              onClick={() => setSelectedUserId(user)}
-              className={`p-2 cursor-pointer rounded ${
-                selectedUserId === user ? 'bg-blue-200' : 'hover:bg-blue-100'
-              }`}
-            >
-              {user}
-            </div>
-          ))}
+        <UsersList users={users} setSelectedUserId={setSelectedUserId} />
       </div>
 
       {/* Chat Area */}
       <div className='flex-1 p-4'>
         {selectedUserId ? (
-          <ChatRoomProvider
-            name={roomName}
-            release={true}
-            attach={true}
-            clientId={currentUserId}
-          >
+          <ChatRoomProvider name={roomName} release={true} attach={true}>
             <ChatBox currentUserId={currentUserId} />
             <ConnectionStatus />
             <RoomStatus />
