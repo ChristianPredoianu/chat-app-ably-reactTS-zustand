046b19eeaf44ec93c036766e39dfef845f0ada57
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-13 12:07:40 +0200

    work

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from '@/App';
+import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <div className='min-h-screen bg-gray-200'>
+        <App />
+      </div>
     </BrowserRouter>
   </React.StrictMode>
 );
