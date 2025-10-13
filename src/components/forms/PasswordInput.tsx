6328537c14ad54fdd7a06474aefdd7fb8953ef91
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-13 14:11:48 +0200

    work

--- src/components/forms/PasswordInput.tsx
+++ src/components/forms/PasswordInput.tsx
@@ -1,3 +1,31 @@
-export default function PasswordInput() {
-  return <h1>Hello PasswordInput</h1>;
+import type { InputHTMLAttributes } from 'react';
+import { CiLock } from 'react-icons/ci';
+
+type PasswordInputProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  name: string;
+};
+
+export default function TextInput({
+  label,
+  name,
+  type = 'password',
+  ...props
+}: PasswordInputProps) {
+  return (
+    <label className='flex flex-col gap-1'>
+      <span className='text-xs mb-2'>{label}</span>
+      <div className='relative'>
+        <input
+          className='border rounded-lg px-10 py-2 focus:outline-none focus:ring focus:border-primary w-full placeholder:text-red-900  placeholder:text-sm'
+          name={name}
+          type={type}
+          {...props}
+        />
+        <span className='absolute left-3 top-3 -translate-y-1/2 text-gray-400 pointer-events-none'>
+          <CiLock />
+        </span>
+      </div>
+    </label>
+  );
 }
