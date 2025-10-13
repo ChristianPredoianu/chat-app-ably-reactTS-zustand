921e06748570c3cf08c4e0af3daf5a3f2ef60bb7
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-13 14:45:12 +0200

    work

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -13,6 +13,7 @@ export default function SignInForm() {
         required
       />
       <PasswordInput label='Password' name='password' placeholder='Password' required />
+      <span className='text-center py-4 text-blue-400 text-sm'>Forgot Password?</span>
       <FormButton>Sign In</FormButton>
     </form>
   );
