4be683f0fa28557d95e465744a6162741bcc0ce9
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-13 14:11:18 +0200

    work

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,7 +1,17 @@
+import { IoChatbubbleEllipsesOutline } from 'react-icons/io5';
+import SignInForm from '@/components/forms/SignInForm';
+
 export default function LoginPage() {
   return (
-    <div className=''>
-      <h1 className='text-red-500'>Login Page</h1>
-    </div>
+    <main className='container mx-auto px-2 '>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <div className='flex flex-col items-center justify-center gap-2'>
+          <IoChatbubbleEllipsesOutline className='text-blue-500' size={40} />
+          <h1 className='text-xl font-medium'>ChatApp</h1>
+        </div>
+        <h2 className='text-2xl font-bold'>Welcome Back!</h2>
+        <SignInForm />
+      </div>
+    </main>
   );
 }
