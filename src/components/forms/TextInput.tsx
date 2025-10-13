ff8340beef657787fd1eef94f7f59673b3c8c914
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-13 14:11:33 +0200

    work

--- src/components/forms/TextInput.tsx
+++ src/components/forms/TextInput.tsx
@@ -1,3 +1,31 @@
-export default function TextInput() {
-  return <h1>Hello TextInput</h1>;
+import type { InputHTMLAttributes } from 'react';
+import { FaRegUserCircle } from 'react-icons/fa';
+
+type TextInputProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  name: string;
+};
+
+export default function TextInput({
+  label,
+  name,
+  type = 'text',
+  ...props
+}: TextInputProps) {
+  return (
+    <label className='flex flex-col gap-1'>
+      <span className='text-xs mb-2'>{label}</span>
+      <div className='relative'>
+        <input
+          className='border rounded-lg px-10 py-2 focus:outline-none focus:ring focus:border-primary w-full placeholder:text-red-900  placeholder:text-sm'
+          name={name}
+          type={type}
+          {...props}
+        />
+        <span className='absolute left-3 top-3 -translate-y-1/2 text-gray-400  pointer-events-none'>
+          <FaRegUserCircle />
+        </span>
+      </div>
+    </label>
+  );
 }
