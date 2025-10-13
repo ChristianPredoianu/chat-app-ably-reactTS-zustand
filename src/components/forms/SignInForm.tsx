82c5ba7d97a1d42edde8908db8ccf6690ea63fa3
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-13 14:11:41 +0200

    work

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -1,3 +1,19 @@
+import TextInput from '@/components/forms/TextInput';
+import PasswordInput from '@/components/forms/PasswordInput';
+import FormButton from '@/components/buttons/FormButton';
+
 export default function SignInForm() {
-  return <h1>Hello SignInForm</h1>;
+  return (
+    <form className='flex flex-col gap-4'>
+      <TextInput
+        label='Email Address'
+        name='email'
+        type='text'
+        placeholder='Email Address'
+        required
+      />
+      <PasswordInput label='Password' name='password' placeholder='Password' required />
+      <FormButton>Sign In</FormButton>
+    </form>
+  );
 }
