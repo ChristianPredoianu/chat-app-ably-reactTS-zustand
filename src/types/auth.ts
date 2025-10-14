0fb17a856401467a63de4cfce9e80c7f99b596d3
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-14 12:14:49 +0200

    work

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -2,4 +2,13 @@ export type SignInState = {
   error?: string;
   success?: boolean;
   message?: string;
+  fieldErrors?: {
+    email?: string;
+    password?: string;
+  };
+};
+
+export type FieldErrors = {
+  email?: string;
+  password?: string;
 };
