71fe640ac09eb2946b8b3a5abe5e8eccce8118cb
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-14 11:22:01 +0200

    work

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,20 +1,38 @@
-import { IoChatbubbleEllipsesOutline } from 'react-icons/io5';
+import { useState } from 'react';
 import SignInForm from '@/components/forms/SignInForm';
+import SignUpForm from '@/components/forms/SignUpForm';
+import { IoChatbubbleEllipsesOutline } from 'react-icons/io5';
 
 export default function LoginPage() {
+  const [activeForm, setActiveForm] = useState<'signIn' | 'signUp'>('signIn');
+
+  const inputComponents = {
+    signIn: <SignInForm />,
+    signUp: <SignUpForm />,
+  };
+
+  function toggleForm() {
+    setActiveForm((prevForm) => (prevForm === 'signIn' ? 'signUp' : 'signIn'));
+  }
+
   return (
     <main className='container mx-auto px-6 min-h-screen flex flex-col justify-between py-10'>
       <div className='flex flex-col items-center justify-start gap-2 '>
         <IoChatbubbleEllipsesOutline className='text-blue-500' size={40} />
         <h1 className='text-xl font-medium'>ChatApp</h1>
       </div>
-      <h2 className='text-2xl font-bold text-center '>Welcome Back!</h2>
-
+      <h2 className='text-2xl font-bold text-center -mt-10'>
+        {activeForm === 'signIn' ? 'Welcome Back!' : 'Create Account'}
+      </h2>
       <div className='flex flex-col items-center gap-4 '>
-        <SignInForm />
+        {inputComponents[activeForm]}
         <p className='text-center py-4  text-sm'>
-          Don’t have an account?
-          <span className='text-blue-400'> SignUp</span>
+          {activeForm === 'signIn'
+            ? 'Don’t have an account?'
+            : 'Already have an account?'}
+          <span className='text-blue-400' onClick={toggleForm}>
+            {activeForm === 'signIn' ? 'Sign Up' : 'Sign In'}
+          </span>
         </p>
       </div>
       <div />
