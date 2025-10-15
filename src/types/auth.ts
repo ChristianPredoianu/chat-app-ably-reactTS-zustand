b2e897e3244b05dfa2f1131c49ac255b0ec45a96
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-15 12:02:58 +0200

    added types

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -12,3 +12,22 @@ export type FieldErrors = {
   email?: string;
   password?: string;
 };
+
+export type SignUpState = {
+  error?: string;
+  success?: boolean;
+  message?: string;
+  fieldErrors?: {
+    email?: string;
+    confirmEmail?: string;
+    password?: string;
+    confirmPassword?: string;
+  };
+};
+
+export type SignUpFieldErrors = {
+  email?: string;
+  confirmEmail?: string;
+  password?: string;
+  confirmPassword?: string;
+};
