f5b23416bd703ba70cf7ab0617953ee974df2458
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-15 12:31:20 +0200

    renamed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from '@/components/pages/LoginPage';
-import ChatPage from '@/components/pages/ChatPage';
+import DashboardPage from '@/components/pages/DashboardPage';
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<LoginPage />} />
-      <Route path='/chat/:userId' element={<ChatPage />} />
+      <Route path='/chat/:userId' element={<DashboardPage />} />
     </Routes>
   );
 }
