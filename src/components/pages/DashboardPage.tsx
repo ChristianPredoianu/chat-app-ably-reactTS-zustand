7988af42941cd0539897a5fc12e34996af06522a
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-15 14:23:26 +0200

    work

--- src/components/pages/DashboardPage.tsx
+++ src/components/pages/DashboardPage.tsx
@@ -1,3 +1,30 @@
-export default function ChatPage() {
-  return <h1>Hello chatPage</h1>;
+import useToggle from '@/hooks/ui/useToggle';
+import useCloseOnResize from '@/hooks/ui/useCloseOnResize';
+import AppLogo from '@/components/ui/AppLogo';
+import ContactsMenu from '@/components/menus/ContactsMenu';
+import Hamburger from '@/components/ui/Hamburger';
+
+export default function DashboardPage() {
+  const {
+    isOpen: isContactsMenuOpen,
+    toggle: toggleContactsMenu,
+    close: closeContactsMenu,
+  } = useToggle(false);
+
+  useCloseOnResize(closeContactsMenu, 1024);
+
+  return (
+    <div className='container mx-auto px-6'>
+      <header className='py-4'>
+        <nav className='flex items-center justify-between'>
+          <Hamburger onClick={toggleContactsMenu} />
+          <AppLogo />
+        </nav>
+      </header>
+      <main>
+        <p className='text-gray-700'>Welcome to your dashboard!</p>
+      </main>
+      <ContactsMenu isOpen={isContactsMenuOpen} onClose={closeContactsMenu} />
+    </div>
+  );
 }
