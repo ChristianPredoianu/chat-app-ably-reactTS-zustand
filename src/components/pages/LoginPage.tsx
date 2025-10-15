4c589301276764a8bed9b7bf58642aa0066a1d77
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-15 11:25:14 +0200

    removed import

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import SignInForm from '@/components/forms/SignInForm';
 import SignUpForm from '@/components/forms/SignUpForm';
-import FormButton from '@/components/buttons/FormButton';
 import { IoChatbubbleEllipsesOutline } from 'react-icons/io5';
 
 export default function LoginPage() {
