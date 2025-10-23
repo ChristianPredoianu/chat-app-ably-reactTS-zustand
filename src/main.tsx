b7e95bd23540d50d79e7f7b6c98bee9f274f7858
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-23 14:34:42 +0200

    work

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
+// main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from '@/App';
+import AblyChatProvider from '@/lib/ably/AblyProvider';
 import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <div className='min-h-screen bg-neutral-100'>
-        <App />
-      </div>
+      <AblyChatProvider>
+        <div className='min-h-screen bg-neutral-100'>
+          <App />
+        </div>
+      </AblyChatProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
