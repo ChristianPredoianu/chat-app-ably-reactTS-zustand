054b4911b21abd3c6b90fae986cece7e8fb4e17b
Christian Predoianu <christianpredoianu@yahoo.com>
2025-10-28 11:38:24 +0100

    added error route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from '@/components/pages/LoginPage';
 import DashboardPage from '@/components/pages/DashboardPage';
+import ErrorPage from '@/components/pages/ErrorPage';
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<LoginPage />} />
       <Route path='/dashboard/:userId' element={<DashboardPage />} />
+      <Route path='*' element={<ErrorPage />} />
     </Routes>
   );
 }
