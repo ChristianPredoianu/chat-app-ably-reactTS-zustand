584153cdb7f354d0377490a64e78216e050bc580
Christian Predoianu <christianpredoianu@yahoo.com>
2025-11-17 13:17:46 +0100

    work

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,15 @@
+import { Client, Account, Databases, Storage, Realtime, ID } from 'appwrite';
+
+const client = new Client();
+
+client
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const realtime = new Realtime(client);
+export { ID };
+
+export default client;
