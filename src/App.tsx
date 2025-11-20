1d8d1ccc4026ecca9cab1e8c7bd23cd3910c2c9b
Christian Predoianu <christianpredoianu@yahoo.com>
2025-11-20 11:21:36 +0100

    work

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from '@/pages/LoginPage';
-
 import DashboardPage from '@/pages/DashboardPage';
 import ErrorPage from '@/pages/ErrorPage';
+import ProtectedLayout from '@/components/routes/ProtectedLayout';
+import PublicRoute from '@/components/routes/PublicRoute'; // Add this
 
 export default function App() {
   return (
     <Routes>
-      <Route path='/' element={<LoginPage />} />
-      <Route path='/dashboard/:userId' element={<DashboardPage />} />
+      <Route
+        path='/'
+        element={
+          <PublicRoute>
+            {' '}
+            {/* Wrap LoginPage with PublicRoute */}
+            <LoginPage />
+          </PublicRoute>
+        }
+      />
+
+      {/* All protected routes under one layout */}
+      <Route element={<ProtectedLayout />}>
+        <Route path='/dashboard/:userId' element={<DashboardPage />} />
+        {/* Add future protected routes here */}
+      </Route>
+
       <Route path='*' element={<ErrorPage />} />
     </Routes>
   );
