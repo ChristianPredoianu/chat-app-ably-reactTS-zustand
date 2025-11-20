5688a0dce7baedd6a00aea0e23bbe1bc776b4cbd
Christian Predoianu <christianpredoianu@yahoo.com>
2025-11-20 11:23:56 +0100

    work

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -1,11 +1,14 @@
 import Hamburger from '@/components/ui/Hamburger';
 import PrimaryButton from '@/components/buttons/PrimaryButton';
+import { useAuth } from '@/hooks/auth/useAuth';
 
 type HeaderProps = {
   onMenuToggle: () => void;
 };
 
 export default function Header({ onMenuToggle }: HeaderProps) {
+  const { signOut } = useAuth();
+
   return (
     <header className='bg-white/80 backdrop-blur-sm border-b border-gray-200/60 '>
       <div className='container mx-auto px-6'>
@@ -14,7 +17,7 @@ export default function Header({ onMenuToggle }: HeaderProps) {
             <Hamburger onClick={onMenuToggle} />
           </div>
           <div className='flex items-center space-x-4'>
-            <PrimaryButton>Sign Out</PrimaryButton>
+            <PrimaryButton onClick={signOut}>Sign Out</PrimaryButton>
           </div>
         </nav>
       </div>
